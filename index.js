d0ea86a8f9b6d3f3c1b8938b5a5187b16380dc38
Mbat1291 <marilyn.bates91@gmail.com>
2025-11-04 20:49:37 -0600

    saves
    I saved my css and javascript pages.

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-let homeScore = document.getElementById("home-score")
-let guestScore = document.getElementById("guest-score")
-let homeCount = 0
-let guestCount = 0
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+let homeCount = 0;
+let guestCount = 0;
 
-function resetGame () {
-    homeCount = homeScore.textContent = 0
-   guestCount = guestScore.textContent = 0
+function resetGame() {
+  homeCount = homeScore.textContent = 0;
+  guestCount = guestScore.textContent = 0;
 }
-function addOneHome () {
-    homeCount += 1
-    homeScore.textContent = homeCount
+function addOneHome() {
+  homeCount += 1;
+  homeScore.textContent = homeCount;
 }
-function addTwoHome () {
-    homeCount += 2
-    homeScore.textContent = homeCount
+function addTwoHome() {
+  homeCount += 2;
+  homeScore.textContent = homeCount;
 }
-function addThreeHome () {
-    homeCount += 3
-    homeScore.textContent = homeCount
+function addThreeHome() {
+  homeCount += 3;
+  homeScore.textContent = homeCount;
 }
 
-function addOneGuest () {
-       guestCount += 1
-    guestScore.textContent = guestCount
+function addOneGuest() {
+  guestCount += 1;
+  guestScore.textContent = guestCount;
 }
-function addTwoGuest () {
-        guestCount += 2
-    guestScore.textContent = guestCount
+function addTwoGuest() {
+  guestCount += 2;
+  guestScore.textContent = guestCount;
 }
-function addThreeGuest () {
-       guestCount += 3
-    guestScore.textContent = guestCount
+function addThreeGuest() {
+  guestCount += 3;
+  guestScore.textContent = guestCount;
 }
